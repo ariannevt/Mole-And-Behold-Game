cd1b35cb1b742d55e170fa720c8677dbdf13d705
Arianne Villavicencio <agv2037@nyu.edu>
2025-05-05 14:20:06 +0400

    improved sketch

--- Assets/Scenes/MixAndMatch/Scripts/GameController.cs
+++ Assets/Scenes/MixAndMatch/Scripts/GameController.cs
@@ -9,12 +9,31 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private Sprite bgImage;
 
+    public Sprite[] puzzles;
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
     public List<Button> btns = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+    void Awake() {
+        puzzles = Resources.LoadAll<Sprite>("Sprites/au");
+    }
 
     void Start()
     {
         GetButtons();
         AddListeners();
+        AddGamePuzzles();
     }
 
     void GetButtons(){
@@ -23,6 +42,21 @@ public class GameController : MonoBehaviour
         for(int i=0; i<objects.Length; i++){
             btns.Add(objects[i].GetComponent<Button>());
             btns[i].image.sprite = bgImage;
+        }
+    }
+
+    void AddGamePuzzles() {
+        int looper = btns.Count;
+        int index = 0;
+
+        for(int i=0; i<looper; i++){
+            if(index == looper / 2) {
+                index = 0;
+            }
+
+            gamePuzzles.Add(puzzles[index]);
+
+            index++;
         }
     }
 
